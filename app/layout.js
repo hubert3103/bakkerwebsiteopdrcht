da3ba425c4cde862cb039e5df5a9b40eb15a6616
hubert3103 <hubert3103@gmail.com>
2025-06-25 16:40:39 +0200

    Favicon issues oplossen

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,3 @@
-
-
 import './globals.css'
 import ClientLayout from './ClientLayout'
 
@@ -45,6 +43,7 @@ export default function RootLayout({ children }) {
         <link rel="apple-touch-icon" href="/icons/icon-192x192.png" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+        <link rel="icon" href="/favicon.ico" />
       </head>
       <ClientLayout>
         {children}
